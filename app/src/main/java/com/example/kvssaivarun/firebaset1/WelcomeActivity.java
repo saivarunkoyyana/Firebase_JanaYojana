d2b306eaefbea79892ce123bb5729470b38a8d69
saivarunkoyyana <sai.varun170598@gmail.com>
2018-11-17 21:44:03 +0530

    Update WelcomeActivity.java

--- app/src/main/java/com/example/kvssaivarun/firebaset1/WelcomeActivity.java
+++ app/src/main/java/com/example/kvssaivarun/firebaset1/WelcomeActivity.java
@@ -5,5 +5,26 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class WelcomeActivity extends AppCompatActivity {
+
+  +public class WelcomeActivity extends AppCompatActivity{
+    private static  int SPlASH_TIME_OUT = 4000;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_welcome );
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                Intent welcomeintent= new Intent( WelcomeActivity.this,MainActivity.class );
+                startActivity( welcomeintent );
+            }
+        },SPlASH_TIME_OUT );
+
+
+    }
+}
+
    
